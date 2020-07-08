e0d47c0f0a359e13a0be17b1bf3c52e64143ddf8
Rise <rise.erpelding@gmail.com>
2020-07-08 08:15:37 -0500

    skewed headers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,10 @@ import { SectionTitle } from '../../utils/headings';
 
 const Header = (props) => (
   <header>
-    <SectionTitle title={props.section} />
+    <div className="section-title__container">
+      <SectionTitle title={props.section} />
+    </div>
+
     <svg
       className="header__svg" 
       xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
